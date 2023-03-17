fb1aee3d2f2b0941d60d8fa10fc3b561d326a57c
nilswg <nilsonweng@gmail.com>
2023-03-18 03:17:06 +0800

    [update] peerServer

--- server/deno/peerServer.ts
+++ server/deno/peerServer.ts
@@ -1,15 +1,29 @@
 // @ts-check
 
-import { PeerServer } from './lib/peer/peer.min.ts'
-
 console.log('DENO_ENV: ', Deno.env.get('DENO_ENV'))
 
-const PORT = Deno.env.get('DENO_ENV') === 'production' ? 80 : 4001
+const isProd = Deno.env.get('DENO_ENV') === 'production'
+const PORT = isProd ? 80 : 4001
+const BASE_URL = isProd ? process.env['DENO_BASE_URL'] : 'http://localhost:3000' // 後面的 "/"不用加
 
-const peerServer = PeerServer({
-  port: PORT,
-  path: '/peerjs',
+import express from './lib/express/express.min.ts'
+import { ExpressPeerServer } from './lib/peer/peer.min.ts'
+import { createServer } from 'https://deno.land/std@0.166.0/node/http.ts'
+
+const app = express()
+const httpServer = createServer(app)
+const peerServer = ExpressPeerServer(httpServer)
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', BASE_URL)
+  next()
 })
+
+app.get('/', (req, res) => {
+  res.status(200).json({ health: 'ok' })
+})
+
+app.use('/peerjs', peerServer)
 
 peerServer.on('connection', (client) => {
   console.log(`使用者連線: Id: ${client.getId().slice(0, 10)}`)
@@ -19,4 +33,4 @@ peerServer.on('disconnect', (client) => {
   console.log(`使用者離線: Id: ${client.getId().slice(0, 10)}`)
 })
 
-console.log(`listening on ${PORT}`)
+httpServer.listen(PORT, () => console.log(`listening on ${PORT}`))
--- server/node/peerServer.js
+++ server/node/peerServer.js
@@ -1,16 +1,22 @@
 // @ts-check
 console.log('NODE_ENV: ', process.env['NODE_ENV'])
 
-const PORT = process.env['NODE_ENV'] === 'production' ? 80 : 4001
+const isProd = process.env['NODE_ENV'] === 'production';
+const PORT = isProd ? 80 : 4001
+const BASE_URL = isProd ? process.env['NODE_BASE_URL'] : 'http://localhost:3000' // 後面的 "/"不用加
 
 const express = require('express')
-const app = express()
-
 const http = require('http')
-const httpServer = http.createServer(app)
-
 const { ExpressPeerServer } = require('peer')
+
+const app = express()
+const httpServer = http.createServer(app)
 const peerServer = ExpressPeerServer(httpServer)
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", BASE_URL);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({ health: 'ok' })
